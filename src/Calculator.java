b2807ace39df5fc1e2f3799585e21d8a1a3e43ad
jkowalska777@gmai.com <jkowalksa777@gmail.com>
2018-05-14 15:53:16 +0200

    just code

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    int substract(int a, int c) {
+        return a - c;
+    }
+
+    double substract(double a, double c) {
+        return a - c;
+    }
+
+}
